f22abf6a5b8b69211337bdebcadfe137134349ea
kaikisk <kueobei@gmail.com>
2019-08-12 17:12:12 +0900

    commit

--- camera.js
+++ camera.js
@@ -1,4 +1,5 @@
 var imageCapture;
+var tempImage;
 var video = document.getElementById("myVideo"); // 適当にvideoタグのオブジェクトを取得
 var constrains = { video: true, audio: false }; // 映像・音声を取得するかの設定
 
@@ -16,9 +17,23 @@ navigator.mediaDevices.getUserMedia(constrains)
 function takePhoto() {
     imageCapture.takePhoto().then(blob => {
         console.log('Photo taken: ' + blob.type + ', ' + blob.size + 'B');
+        tempImage = blob;
 
         const image = document.querySelector('img');
-        // image.src = URL.createObjectURL(blob);
+        image.src = URL.createObjectURL(blob);
     })
     .catch(err => console.error('takePhoto() failed: ', err));
+}
+
+function save(){
+    var keyName = document.getElementById("key").value; // キー名を取ってくる
+    var ts = db.transaction(["store1"], "readwrite");
+    var store = ts.objectStore("store1");
+    var request = store.put({mykey: keyName, mayvalue: blob});
+    request.onsuccess = function(){
+        console.log("success put img");
+    }
+    request.onerror = function(){
+        console.log("error put img");
+    }
 }
