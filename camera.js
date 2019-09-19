7a5632f1ae60113c19fa387d0fa362adfc97f829
kaikisk <kueobei@gmail.com>
2019-09-20 03:06:15 +0900

    commit

--- camera.js
+++ camera.js
@@ -1,7 +1,7 @@
 var imageCapture;
 var tempImage;
 var video = document.getElementById("myVideo"); // 適当にvideoタグのオブジェクトを取得
-var constrains = { video:{facingMode: "environment"}, audio: false }; // 映像・音声を取得するかの設定
+var constrains = { video:{facingMode: { exact: "environment" }}, audio: false }; // 映像・音声を取得するかの設定
 
 navigator.mediaDevices.enumerateDevices()
 .then(devices => {
@@ -29,6 +29,8 @@ function gotStream(stream) {
 
     const track = stream.getVideoTracks()[0];
     imageCapture = new ImageCapture(track);
+
+    alert("succsess");
 }
 
 // function changeVideo(){
