920ba8dac6a83889760de260fa47e3081632f20e
kaikisk <kueobei@gmail.com>
2019-09-27 04:23:25 +0900

    commit

--- camera.js
+++ camera.js
@@ -1,8 +1,9 @@
 var imageCapture;
 var tempImage;
 var video = document.getElementById("myVideo"); // 適当にvideoタグのオブジェクトを取得
-// var constrains = { video:{facingMode: { exact: "environment" }}, audio: false }; // 映像・音声を取得するかの設定
-var constrains = { video: true, audio: false }; // 映像・音声を取得するかの設定
+var canvas = document.getElementById('canvas');
+var constrains = { video:{facingMode: { exact: "environment" }}, audio: false }; // 映像・音声を取得するかの設定、リアカメラ
+// var constrains = { video: true, audio: false }; // 映像・音声を取得するかの設定
 
 navigator.mediaDevices.getUserMedia(constrains)
 .then(gotStream).catch(function(err) {
@@ -38,15 +39,31 @@ function gotStream(stream) {
 //     });
 // }
 
-function takePhoto() {
-    imageCapture.takePhoto().then(blob => {
-        console.log('Photo taken: ' + blob.type + ', ' + blob.size + 'B');
-        tempImage = blob;
+// function takePhoto() {
+//     imageCapture.takePhoto().then(blob => {
+//         console.log('Photo taken: ' + blob.type + ', ' + blob.size + 'B');
+//         tempImage = blob;
 
-        const image = document.querySelector('img');
-        image.src = URL.createObjectURL(blob);
-    })
-    .catch(err => console.error('takePhoto() failed: ', err));
+//         const image = document.querySelector('img');
+//         image.src = URL.createObjectURL(blob);
+//     })
+//     .catch(err => console.error('takePhoto() failed: ', err));
+// }
+
+function take_picture() {
+    //videoのstreamをcanvasに書き出す方法
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    //videoの縦幅横幅を取得
+    var w = video.offsetWidth;
+    var h = video.offsetHeight;    
+    canvas.setAttribute("width", w);
+    canvas.setAttribute("height", h);
+    ctx.drawImage(video, 0, 0, w, h);
+
+    //canvasを更にimgに書き出す方法
+    var img = document.querySelector('img');
+    img.src = canvas.toDataURL('image/png');
 }
 
 function save(){
